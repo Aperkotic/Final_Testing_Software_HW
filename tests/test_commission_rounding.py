803ce9b800ec20c4adf40468c8cc0c322cf56350
Aperkotic <Aperkotic@gmail.com>
2026-04-28 21:28:03 +0300

    First commit

---
+++ tests/test_commission_rounding.py
@@ -0,0 +1,35 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+@pytest.fixture
+def driver():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
+    driver = webdriver.Chrome(options=options)
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
+
+def test_commission_rounding(driver):
+    driver.get('http://localhost:8000/?balance=30000&reserved=20001')
+
+    button_ruble = driver.find_element(By.XPATH, "//div[@role='button'][1]")
+    button_ruble.click()
+
+    card_number = driver.find_element(By.XPATH, "//input[@placeholder='0000 0000 0000 0000']")
+    card_number.clear()
+    card_number.send_keys('1234567812345678')
+
+    sum_amount = driver.find_element(By.XPATH, "//input[@placeholder='1000']")
+    sum_amount.clear()
+    sum_amount.send_keys('9090')
+
+    actual_commission_value = driver.find_element(By.ID, "comission")
+    expected_commission_value = 909
+
+    assert actual_commission_value == expected_commission_value
